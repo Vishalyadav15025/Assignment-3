eceb5e34596b6a1a6bac2a94aa419451c91cbae2
RodneyLey <rodneymwalr@gmail.com>
2023-11-29 10:03:35 -0400

    updated semantic HTML

--- DOM Manipulation/domMain.js
+++ DOM Manipulation/domMain.js
@@ -1,20 +1,33 @@
-let h2Element = document.getElementById("Food");
-h2Element.childNodes[0].nodeValue = 'The Get Methods'
-h2Element.style.color = "blue";
+// let h2Element = document.getElementById("Food");
+// h2Element.childNodes[0].nodeValue = 'The Get Methods'
+// h2Element.style.color = "blue";
 
-let pElements = document.getElementsByTagName('p'); //HTML collection
-    for (let pElement of pElements) {
-            pElement.style.color = 'brown'
-            };
+// let pElements = document.getElementsByTagName('p'); //HTML collection
+//     for (let pElement of pElements) {
+//             pElement.style.color = 'brown'
+//             };
 
-let customDivs = document.getElementsByClassName('custom-bg');
-    for (let element of customDivs){
-        element.style.backgroundColor = 'yellow'
-        };
+// let customDivs = document.getElementsByClassName('custom-bg');
+//     for (let element of customDivs){
+//         element.style.backgroundColor = 'yellow'
+//         };
 
-let pElementsInDiv = document.querySelectorAll("div p")
-        for(let para of pElementsInDiv){
-            para.style.fontWeight = 'bold'
-        }
-let specialP = document.querySelector('#shoes');
-specialP.style.textDecoration = 'underline'
+// let pElementsInDiv = document.querySelectorAll("div p")
+//         for(let para of pElementsInDiv){
+//             para.style.fontWeight = 'bold'
+//         }
+// let specialP = document.querySelector('#shoes');
+// specialP.style.textDecoration = 'underline'
+
+// let a = combination(10, 20);
+// document.getElementById("test").innerHTML = a;
+
+// function combination(b,c){
+//     return (b*c)
+// }
+
+let a = function combination (b, c){
+    return b * c
+};
+
+document.getElementById("test").innerHTML = a (6, 10)
