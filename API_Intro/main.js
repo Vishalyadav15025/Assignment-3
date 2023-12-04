4722237c2b2883c2d66030d5bea9b228bcde05e4
RodneyLey <rodneymwalr@gmail.com>
2023-12-03 21:40:54 -0400

    Added an element using JS

---
+++ API_Intro/main.js
@@ -0,0 +1,7 @@
+function getAllNames() {
+    const namesDataBase = {
+        names : ["Rodney","Yash","Debra","Brian"]
+    }
+    return namesDataBase;
+};
+console.log(getAllNames())
--- Assignment_02/main.js
+++ Assignment_02/main.js
@@ -1,4 +1,4 @@
-var x = document.getElementById("button");
+let x = document.getElementById("button");
 x.addEventListener("click", toggleDiv);
 // x.addEventListener("click", displayAlert);
 
@@ -21,5 +21,28 @@ function toggleDiv() {
     }
   }
   
+// changinh the HTML text of the test id
+  document.getElementById("test").innerHTML = '"In the world of bread and pastries, magic happens. Indulging in the simplicity and beauty of freshly baked goodness. A bakery is where dreams rise and delicious memories are made."'
 
-  document.getElementById("test").innerHTML = "Rodney Mwale"
+
+// adding 2 elements then removing one
+  document.getElementById("beforeFooter").classList.add("newStyle1", "newStyle2")
+  document.getElementById("beforeFooter").classList.remove("newStyle2")
+
+// Create a new element and add it to the page 
+let myNewElement = document.createElement("ul");
+
+sponser = ["UberEats", "Shoppers", "Walmart"]
+
+// create list element and map it to the content of the list(sponser)
+sponser.forEach(name => {
+    let liElement = document.createElement("li");
+    liElement.textContent = name;
+    myNewElement.appendChild(liElement);
+});
+
+let footerElement = document.querySelector("footer");
+
+footerElement.parentNode.insertBefore(myNewElement, footerElement)
+
+
